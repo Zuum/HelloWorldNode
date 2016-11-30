6084ab46db463a33ba2648ab66c233c854c6e568
Zuum <djzuumik@gmail.com>
2016-11-30 23:19:00 +0200

    chat

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var handlebars = require('express-handlebars');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.engine('hbs', handlebars({
   layoutsDir: path.join(__dirname, "public/views"),
-  defaultLayout: 'echo',
+  defaultLayout: 'chat',
   extname: '.hbs' }));
 app.set('views', path.join(__dirname, "public/views"));
 app.set('view engine', '.hbs');
@@ -14,14 +16,21 @@ app.set('view engine', '.hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', function(req, res) {
-  res.render('echo', { response: req.body.query });
-});
-
 app.get('/', function (req, res) {
-  res.render('echo', {layout: false});
+  res.render('chat', {layout: false});
 });
 
-app.listen(3000, function () {
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+app.all('/:anything', function (req, res) {
+  res.send('We don\'t have one. Error 404');
+});
+
+
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
