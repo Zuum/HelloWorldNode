1bf2bb54c41fcb31575279e0650a8855c2378177
Zuum <djzuumik@gmail.com>
2016-11-30 20:15:56 +0200

    echo server with get\post

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var handlebars = require('express-handlebars');
+
+app.engine('hbs', handlebars({
+  layoutsDir: path.join(__dirname, "public/views"),
+  defaultLayout: 'echo',
+  extname: '.hbs' }));
+app.set('views', path.join(__dirname, "public/views"));
+app.set('view engine', '.hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function(req, res) {
+  res.render('echo', { response: req.body.query });
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('echo', {layout: false});
 });
 
 app.listen(3000, function () {
